bf039cd8a7ec3ada6bec0cccefe41cdfab52c705
vanshita shah <vanshita.s@simformsolutions.com>
2023-04-18 18:23:01 +0530

    Created App.js and index.js and jsx for text rendering is added

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import 'App.css'
+import img from '../public/img.png'
+const App = () => {
+    return (
+        <div>
+            <h1 className="heading">Hello React</h1>
+        </div>
+    );
+}
+export default App;
